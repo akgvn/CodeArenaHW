2fbb583d1d9152f421358f6ff040472cf332bbbb
akgvn <akgvn@yandex.com>
2019-05-08 21:36:02 +0300

    Adding question is looking better rn

--- htdocs/addquestion.php
+++ htdocs/addquestion.php
@@ -5,37 +5,49 @@ if (isset($_SESSION["username"]) != true) {
     header("Location: login.php");
 }
 
-print_r($_SESSION);
-
-echo "Hey.";
 ?>
 
 <html>
-	<body>
-		<form method = "post">
+
+    <head>
+        <link href="https://getbootstrap.com/docs/4.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://getbootstrap.com/docs/4.3/examples/sign-in/signin.css" rel="stylesheet">
+    </head>
+
+	<body class="text-center">
+    
+		<form method = "post" class="form-signin">
+        <a href="listallusers.php">Go Back to Users List</a>
+        <br><br>
+        <h1 class="h3 mb-3 font-weight-normal">Add Question</h1>
+
+            <div class="form-group">
 			<label>Question Title:<br>
-			<input required name = "question_title" placeholder="Question Title..."></input> <br>
+			<input required name = "question_title" placeholder="Question Title..." class="form-control" ></input> <br>
 			</label>
-
+            </div>
+            <div class="form-group">
 			<label>Question Text:<br>
-			<textarea required name = "question_text" cols = "40" rows = "6" placeholder="Question..."></textarea> <br>
+			<textarea required name = "question_text" cols = "40" rows = "6" placeholder="Question..." class="form-control" ></textarea> <br>
 			</label>
-
+            </div>
+            <div class="form-group">
 			<label>Answer:<br>
-			<input required name = "answer" placeholder="Answer..."></input> <br>
+			<input required name = "answer" placeholder="Answer..." class="form-control" ></input> <br>
 			</label>
 			<br>
-			<input type="submit" value="Submit" />
-		</form>
+            </div>
+            <div class="form-group">
+			
+			<input type="submit" value="Submit Question" class="btn btn-lg btn-primary btn-block" />
+            </div>
+    	</form>
+   
 	</body>
 </html>
 
 <?php
 require "db_connect.php";
-
-echo $_SESSION["username"];
-
-echo "Hey.";
 
 if (array_key_exists("username", $_SESSION)) {
     if (array_key_exists("question_title", $_POST) && array_key_exists("question_text", $_POST) && array_key_exists("answer", $_POST)) {
